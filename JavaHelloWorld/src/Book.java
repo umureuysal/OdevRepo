5f83e4913c315f2ed3cdf6309f629dd0b17f4ded
Umur E. Uysal <98770959+umureuysal@users.noreply.github.com>
2023-06-05 23:18:15 +0300

    Add files via upload

---
+++ JavaHelloWorld/src/Book.java
@@ -0,0 +1,45 @@
+import java.util.*;
+
+public class Book implements Comparable<Book> {
+    private String bookName;
+    private int pageCount;
+    private String authorName;
+    private String publicationDate;
+
+    public Book(String bookName, int pageCount, String authorName, String publicationDate) {
+        this.bookName = bookName;
+        this.pageCount = pageCount;
+        this.authorName = authorName;
+        this.publicationDate = publicationDate;
+    }
+
+    public String getBookName() {
+        return bookName;
+    }
+
+    public int getPageCount() {
+        return pageCount;
+    }
+
+    public String getAuthorName() {
+        return authorName;
+    }
+
+    public String getPublicationDate() {
+        return publicationDate;
+    }
+
+    @Override
+    public int compareTo(Book otherBook) {
+        return this.bookName.compareTo(otherBook.bookName);
+    }
+
+    @Override
+    public String toString() {
+        return "Book{" +
+                "bookName='" + bookName + '\'' +
+                ", pageCount=" + pageCount +
+                ", authorName='" + authorName + '\'' +
+                ", publicationDate='" + publicationDate + '\'' +
+                '}';
+    }
